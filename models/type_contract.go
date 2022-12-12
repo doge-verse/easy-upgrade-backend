5799b5421cee284d0dd77d94e09ea5c5757ba006
FWangZil <acewzk@gmail.com>
2022-12-12 14:48:54 +0800

    feat: uniqueIndex

--- models/type_contract.go
+++ models/type_contract.go
@@ -4,10 +4,10 @@ package models
 type Contract struct {
 	GormModel
 	UserID             uint              `gorm:"NOT NULL" json:"userID"`
-	ProxyAddress       string            `gorm:"NOT NULL" json:"proxyAddress"`
+	ProxyAddress       string            `gorm:"NOT NULL;uniqueIndex:idx_uni;type:varchar(64)" json:"proxyAddress"`
 	ProxyOwner         string            `gorm:"NOT NULL" json:"proxyOwner"`
 	Name               string            `json:"name"`
-	Network            uint              `gorm:"NOT NULL" json:"network"` // chain id
+	Network            uint              `gorm:"NOT NULL;uniqueIndex:idx_uni;type:varchar(64)" json:"network"` // chain id
 	LastUpdate         uint64            `json:"lastUpdate"`
 	Email              string            `json:"email"`
 	ContractHistoryArr []ContractHistory `json:"contractHistoryArr,omitempty"`
--- models/type_user.go
+++ models/type_user.go
@@ -4,7 +4,7 @@ package models
 type User struct {
 	GormModel
 	Name        string     `json:"name"`
-	Address     string     `gorm:"NOT NULL" json:"address"`
+	Address     string     `gorm:"NOT NULL;uniqueIndex:idx_uni;type:varchar(64)" json:"address"`
 	Email       string     `json:"email"`
 	ContractArr []Contract `json:"contractArr,omitempty"`
 }
